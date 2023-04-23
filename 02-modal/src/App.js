106ef99212fa8e8d08dda954d13347aeaa5c9ea4
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-23 10:32:08 +0300

    take out Modal component from App

--- 02-modal/src/App.js
+++ 02-modal/src/App.js
@@ -1,6 +1,27 @@
 import React from 'react'
 import './index.scss'
 
+const Modal = ({ open, setOpen, children }) => (
+	<div className={`overlay animated ${open ? 'show' : ''}`}>
+		<div className='modal'>
+			<svg
+				height='200'
+				viewBox='0 0 200 200'
+				width='200'
+				onClick={() => setOpen(false)}
+			>
+				<title />
+				<path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
+			</svg>
+			{/* {children} */}
+			<img
+				src='https://media.giphy.com/media/fCCCy9QuTtsFM9S88n/giphy-downsized-large.gif'
+				alt='giphy'
+			/>
+		</div>
+	</div>
+)
+
 function App() {
 	const [open, setOpen] = React.useState(false)
 
@@ -9,24 +30,10 @@ function App() {
 			<button onClick={() => setOpen(true)} className='open-modal-btn'>
 				✨ Открыть окно
 			</button>
-
-			<div className={`overlay animated ${open ? 'show' : ''}`}>
-				<div className='modal'>
-					<svg
-						height='200'
-						viewBox='0 0 200 200'
-						width='200'
-						onClick={() => setOpen(false)}
-					>
-						<title />
-						<path d='M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z' />
-					</svg>
-					<img
-						src='https://media.giphy.com/media/fCCCy9QuTtsFM9S88n/giphy-downsized-large.gif'
-						alt='giphy'
-					/>
-				</div>
-			</div>
+			<Modal open={open} setOpen={setOpen}>
+				{/* <h3>This is modal window</h3>
+				<button>Click me</button> */}
+			</Modal>
 		</div>
 	)
 }
