9d0e8fad0f4d241372fa60469d5db7eb07c50b45
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-27 19:31:50 +0300

    add counter for correct answers, fix try again btn & finish the app

--- 03-quiz/src/App.js
+++ 03-quiz/src/App.js
@@ -27,12 +27,16 @@ const questions = [
 	},
 ]
 
-function Result() {
+function Result({ correct }) {
 	return (
 		<div className='result'>
 			<img src='https://cdn-icons-png.flaticon.com/512/2278/2278992.png' />
-			<h2>Вы отгадали 3 ответа из 10</h2>
-			<button>Попробовать снова</button>
+			<h2>
+				Вы отгадали {correct} ответа из {questions.length}
+			</h2>
+			<a href='/'>
+				<button>Попробовать снова</button>
+			</a>
 		</div>
 	)
 }
@@ -62,17 +66,24 @@ function Game({ question, onClickVariant, step }) {
 
 function App() {
 	const [step, setStep] = React.useState(0)
+	const [correct, setCorrect] = React.useState(0)
 	const question = questions[step]
 
 	const onClickVariant = (index) => {
-		console.log(step, index)
 		setStep(step + 1)
+
+		if (index === question.correct) {
+			setCorrect(correct + 1)
+		}
 	}
 
 	return (
 		<div className='App'>
-			<Game step={step} question={question} onClickVariant={onClickVariant} />
-			{/* <Result /> */}
+			{step !== questions.length ? (
+				<Game step={step} question={question} onClickVariant={onClickVariant} />
+			) : (
+				<Result correct={correct} />
+			)}
 		</div>
 	)
 }
