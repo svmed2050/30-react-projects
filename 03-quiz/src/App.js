c2605c3b23d9929b7c19667ce5a321fe82b25896
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-27 19:18:24 +0300

    render question to game component

--- 03-quiz/src/App.js
+++ 03-quiz/src/App.js
@@ -1,60 +1,69 @@
-import './index.scss';
+import React from 'react'
+import './index.scss'
 
 const questions = [
-  {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
-    correct: 0,
-  },
-  {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
-    correct: 1,
-  },
-  {
-    title: 'Что такое JSX?',
-    variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
-    ],
-    correct: 2,
-  },
-];
+	{
+		title: 'React - это ... ?',
+		variants: ['библиотека', 'фреймворк', 'приложение'],
+		correct: 0,
+	},
+	{
+		title: 'Компонент - это ... ',
+		variants: [
+			'приложение',
+			'часть приложения или страницы',
+			'то, что я не знаю что такое',
+		],
+		correct: 1,
+	},
+	{
+		title: 'Что такое JSX?',
+		variants: [
+			'Это простой HTML',
+			'Это функция',
+			'Это тот же HTML, но с возможностью выполнять JS-код',
+		],
+		correct: 2,
+	},
+]
 
 function Result() {
-  return (
-    <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
-    </div>
-  );
+	return (
+		<div className='result'>
+			<img src='https://cdn-icons-png.flaticon.com/512/2278/2278992.png' />
+			<h2>Вы отгадали 3 ответа из 10</h2>
+			<button>Попробовать снова</button>
+		</div>
+	)
 }
 
-function Game() {
-  return (
-    <>
-      <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
-      </div>
-      <h1>Что такое useState?</h1>
-      <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
-      </ul>
-    </>
-  );
+function Game({ question }) {
+	return (
+		<>
+			<div className='progress'>
+				<div style={{ width: '50%' }} className='progress__inner'></div>
+			</div>
+			<h1>{question.title}</h1>
+			<ul>
+				{question.variants.map((text) => (
+					<li key={text}>{text}</li>
+				))}
+			</ul>
+		</>
+	)
 }
 
 function App() {
-  return (
-    <div className="App">
-      <Game />
-      {/* <Result /> */}
-    </div>
-  );
+	const [step, setStep] = React.useState(0)
+	const question = questions[step]
+	console.log(question)
+
+	return (
+		<div className='App'>
+			<Game question={question} />
+			{/* <Result /> */}
+		</div>
+	)
 }
 
-export default App;
+export default App
