ea374746f1b4ac2df2cb1c3f553b42b8a97e6636
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-28 19:34:36 +0300

    add fetch for users

--- 04-users/src/App.js
+++ 04-users/src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React from 'react'
+import './index.scss'
+import { Success } from './components/Success'
+import { Users } from './components/Users'
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
-  return (
-    <div className="App">
-      <Users />
-      {/* <Success /> */}
-    </div>
-  );
+	const [users, setUsers] = React.useState([])
+
+	React.useEffect(() => {
+		fetch(`https://reqres.in/api/users`)
+			.then((res) => res.json())
+			.then((json) => setUsers(json.data))
+			.catch((err) => {
+				console.warn(err)
+				alert('An error occured while fetching users')
+			})
+	}, [])
+
+	return (
+		<div className='App'>
+			<Users />
+			{/* <Success /> */}
+		</div>
+	)
 }
 
-export default App;
+export default App
