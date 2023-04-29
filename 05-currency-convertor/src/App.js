4f3928d3b3758628cb2789f639b900f75f6ceec9
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-29 16:01:30 +0300

    add fetch from API with currencies

--- 05-currency-convertor/src/App.js
+++ 05-currency-convertor/src/App.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React from 'react'
+import { Block } from './Block'
+import './index.scss'
+
+/* 
+https://apilayer.com/marketplace/exchangerates_data-api
+*/
 
 function App() {
-  return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
-  );
+	const [fromCurrency, setFromCurrency] = React.useState('RUB')
+	const [toCurrency, setToCurrency] = React.useState('USD')
+	const [rates, setRates] = React.useState({})
+	const apikey = 'MGQcCrvbmwnmNKXujVcUnRM8hiV9XLDt'
+	const requestOptions = {
+		method: 'GET',
+		headers: {
+			apikey,
+		},
+	}
+
+	React.useEffect(() => {
+		const url =
+			'https://api.apilayer.com/exchangerates_data/convert?to=EUR&from=USD&amount=20'
+		fetch(url, requestOptions)
+			.then((res) => res.json())
+			.then((json) => {
+				setRates(json.result)
+				console.log(json.result)
+			})
+			.catch((err) => {
+				console.warn(err)
+				alert('Failed to get information')
+			})
+	}, [])
+
+	return (
+		<div className='App'>
+			<Block
+				value={0}
+				currency={fromCurrency}
+				onChangeCurrency={setFromCurrency}
+			/>
+			<Block value={0} currency={toCurrency} onChangeCurrency={setToCurrency} />
+
+			<Block value={0} currency='USD' />
+		</div>
+	)
 }
 
-export default App;
+export default App
