0b23808c5757db079651eea81ed3b7149ae1654b
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-30 20:51:44 +0300

    fetching collections from mockapi api

--- 06-photos/src/App.js
+++ 06-photos/src/App.js
@@ -11,7 +11,19 @@ https://644ea6b91b4567f4d58d0dda.mockapi.io/photo_collections
 function App() {
 	const [collections, setCollections] = React.useState([])
 
-	React.useEffect(() => {})
+	React.useEffect(() => {
+		const url = `https://644ea6b91b4567f4d58d0dda.mockapi.io/photo_collections`
+
+		fetch(url)
+			.then((res) => res.json())
+			.then((json) => {
+				setCollections(json[0].collections)
+			})
+			.catch((err) => {
+				console.warn(err)
+				alert('An error occurred while fetching data')
+			})
+	}, [])
 
 	return (
 		<div className='App'>
@@ -27,15 +39,11 @@ function App() {
 				<input className='search-input' placeholder='Поиск по названию' />
 			</div>
 			<div className='content'>
-				<Collection
-					name='Путешествие по миру'
-					images={[
-						'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-						'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-						'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-						'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-					]}
-				/>
+				{collections.map((obj) => {
+					return (
+						<Collection name={obj.name} images={obj.photos} key={obj.name} />
+					)
+				})}
 			</div>
 			<ul className='pagination'>
 				<li>1</li>
