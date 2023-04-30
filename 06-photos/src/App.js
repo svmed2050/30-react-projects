629b47993471a98005d453a7a0140d719bba9ce3
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-04-30 22:09:40 +0300

    add pagination & complete photos project

--- 06-photos/src/App.js
+++ 06-photos/src/App.js
@@ -18,15 +18,17 @@ const cats = [
 
 function App() {
 	const [categoryId, setCategoryId] = React.useState(0)
+	const [page, setPage] = React.useState(1)
 	const [isLoading, setIsLoading] = React.useState(true)
 	const [searchValue, setSearchValue] = React.useState('')
 	const [collections, setCollections] = React.useState([])
 
 	React.useEffect(() => {
 		setIsLoading(true)
-		const url = `https://644ea6b91b4567f4d58d0dda.mockapi.io/photo_collections?${
-			categoryId ? `category=${categoryId}` : ''
-		}`
+
+		const category = `${categoryId ? `category=${categoryId}` : ''}`
+
+		const url = `https://644ea6b91b4567f4d58d0dda.mockapi.io/photo_collections?${category}&page=${page}&limit=3`
 
 		fetch(url)
 			.then((res) => res.json())
@@ -39,7 +41,7 @@ function App() {
 				alert('An error occurred while fetching data')
 			})
 			.finally(() => setIsLoading(false))
-	}, [categoryId])
+	}, [categoryId, page])
 
 	return (
 		<div className='App'>
@@ -79,9 +81,14 @@ function App() {
 				)}
 			</div>
 			<ul className='pagination'>
-				<li>1</li>
-				<li className='active'>2</li>
-				<li>3</li>
+				{[...Array(3)].map((_, i) => (
+					<li
+						onClick={() => setPage(i + 1)}
+						className={page === i + 1 ? 'active' : ''}
+					>
+						{i + 1}
+					</li>
+				))}
 			</ul>
 		</div>
 	)
