e68a24aa865c063f048e140eaa53fd0c8a7f0737
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-01 21:05:29 +0300

    add start and stop timer functions wit useRef to clearsetInterval

--- 07-pomodoro/src/App.js
+++ 07-pomodoro/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './App.css'
 
 function padTime(time) {
@@ -8,9 +8,11 @@ function padTime(time) {
 export default function App() {
 	const [title, setTitle] = useState('Let the countdown begin!!!')
 	const [timeLeft, setTimeLeft] = useState(5)
+	const intervalRef = useRef(null)
 
 	function startTimer() {
-		setInterval(() => {
+		setTitle(`You are doing great!`)
+		intervalRef.current = setInterval(() => {
 			setTimeLeft((timeLeft) => {
 				if (timeLeft >= 1) return timeLeft - 1
 
@@ -18,6 +20,11 @@ export default function App() {
 				return 0
 			})
 		}, 1000)
+	}
+
+	function stopTimer() {
+		clearInterval(intervalRef.current)
+		setTitle('Keep it up!')
 	}
 
 	const minutes = padTime(Math.floor(timeLeft / 60))
@@ -35,7 +42,7 @@ export default function App() {
 
 			<div className='buttons'>
 				<button onClick={startTimer}>Start</button>
-				<button>Stop</button>
+				<button onClick={stopTimer}>Stop</button>
 				<button>Reset</button>
 			</div>
 		</div>
