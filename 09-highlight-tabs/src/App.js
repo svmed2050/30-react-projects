24d2e9e54c72b1dad547f773a2c4ba33f88e0507
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-03 19:38:55 +0300

    create Tab component with move and hide functions

--- 09-highlight-tabs/src/App.js
+++ 09-highlight-tabs/src/App.js
@@ -1,23 +1,39 @@
 import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-	const [highlightStyle, setHighlightStyle] = useState({ left: 0 })
+function Tab() {
+	const [highlightStyle, setHighlightStyle] = useState({ left: 0, opacity: 0 })
 
+	function moveHighlight(e) {
+		// apdate highlightStyle to move the highlight
+		setHighlightStyle({
+			left: e.nativeEvent.layerX - 150,
+		})
+	}
+
+	function hideHighlight(e) {
+		setHighlightStyle({
+			opacity: 0,
+			left: e.nativeEvent.layerX - 150,
+		})
+	}
+
+	return (
+		<div className='tab' onMouseOut={hideHighlight} onMouseMove={moveHighlight}>
+			<div className='highlight' style={highlightStyle} />
+			<a>Home</a>
+		</div>
+	)
+}
+
+function App() {
 	return (
 		<div className='app'>
 			<div className='browser'>
 				<div className='tabs'>
-					<div className='tab'>
-						<div className='highlight' style={{ left: -100 }} />
-						<a>Home</a>
-					</div>
-					<div className='tab'>
-						<a>About</a>
-					</div>
-					<div className='tab'>
-						<a>Features</a>
-					</div>
+					<Tab />
+					<Tab />
+					<Tab />
 				</div>
 
 				<div className='viewport'>Pages Go Here</div>
