1504dd65ad329d42d2a258f01619f5158bfe748d
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-03 18:43:13 +0300

    add highlight styling & useState for highlight

--- 09-highlight-tabs/src/App.js
+++ 09-highlight-tabs/src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="app">
-      <div className="browser">
-        <div className="tabs">
-          <div className="tab">
-            <a>Home</a>
-          </div>
-          <div className="tab">
-            <a>About</a>
-          </div>
-          <div className="tab">
-            <a>Features</a>
-          </div>
-        </div>
+	const [highlightStyle, setHighlightStyle] = useState({ left: 0 })
 
-        <div className="viewport">Pages Go Here</div>
-      </div>
-    </div>
-  );
+	return (
+		<div className='app'>
+			<div className='browser'>
+				<div className='tabs'>
+					<div className='tab'>
+						<div className='highlight' style={{ left: -100 }} />
+						<a>Home</a>
+					</div>
+					<div className='tab'>
+						<a>About</a>
+					</div>
+					<div className='tab'>
+						<a>Features</a>
+					</div>
+				</div>
+
+				<div className='viewport'>Pages Go Here</div>
+			</div>
+		</div>
+	)
 }
 
-export default App;
+export default App
