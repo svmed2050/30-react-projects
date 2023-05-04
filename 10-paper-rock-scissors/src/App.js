848e5c61b2fa572a083566913e2c3dcaa23fa284
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-04 21:30:09 +0300

    create choice array with data & handler for user choices

--- 10-paper-rock-scissors/src/App.js
+++ 10-paper-rock-scissors/src/App.js
@@ -1,59 +1,71 @@
-import React from 'react';
-import Rock from './icons/Rock';
-import Paper from './icons/Paper';
-import Scissors from './icons/Scissors';
-import './App.css';
+import React, { useState } from 'react'
+import Rock from './icons/Rock'
+import Paper from './icons/Paper'
+import Scissors from './icons/Scissors'
+import './App.css'
+
+const choices = [
+	{ id: 1, name: 'rock', component: Rock },
+	{ id: 2, name: 'paper', component: Paper },
+	{ id: 3, name: 'scissors', component: Scissors },
+]
 
 export default function App() {
-  return (
-    <div className="app">
-      {/* information goes here */}
-      <div className="info">
-        <h2>Rock. Paper. Scissors</h2>
+	const [userChoice, setUserChoice] = useState(null)
 
-        {/* wins vs losses stats */}
-        <div className="wins-losses">
-          <div className="wins">
-            <span className="number">0</span>
-            <span className="text">Wins</span>
-          </div>
+	function handleUserChoice(choice) {
+		const userChoice = choices.find((c) => c.id === choice)
+	}
 
-          <div className="losses">
-            <span className="number">0</span>
-            <span className="text">Losses</span>
-          </div>
-        </div>
-      </div>
+	return (
+		<div className='app'>
+			{/* information goes here */}
+			<div className='info'>
+				<h2>Rock. Paper. Scissors</h2>
 
-      {/* the popup to show win/loss/draw */}
-      {/* <div className="game-state"></div> */}
+				{/* wins vs losses stats */}
+				<div className='wins-losses'>
+					<div className='wins'>
+						<span className='number'>0</span>
+						<span className='text'>Wins</span>
+					</div>
 
-      <div className="choices">
-        {/* choices captions */}
-        <div>You</div>
-        <div />
-        <div>Computer</div>
+					<div className='losses'>
+						<span className='number'>0</span>
+						<span className='text'>Losses</span>
+					</div>
+				</div>
+			</div>
 
-        {/* buttons for my choice */}
-        <div>
-          <button className="rock">
-            <Rock />
-          </button>
-          <button className="paper">
-            <Paper />
-          </button>
-          <button className="scissors">
-            <Scissors />
-          </button>
-        </div>
+			{/* the popup to show win/loss/draw */}
+			{/* <div className="game-state"></div> */}
 
-        <div className="vs">vs</div>
+			<div className='choices'>
+				{/* choices captions */}
+				<div>You</div>
+				<div />
+				<div>Computer</div>
 
-        {/* show the computer's choice */}
-        <div>
-          <button className="computer-choice">?</button>
-        </div>
-      </div>
-    </div>
-  );
+				{/* buttons for my choice */}
+				<div>
+					<button className='rock' onClick={() => handleUserChoice(1)}>
+						<Rock />
+					</button>
+					<button className='paper' onClick={() => handleUserChoice(2)}>
+						<Paper />
+					</button>
+					<button className='scissors' onClick={() => handleUserChoice(3)}>
+						<Scissors />
+					</button>
+				</div>
+
+				<div className='vs'>vs</div>
+
+				{/* show the computer's choice */}
+				<div>
+					<button className='computer-choice'>?</button>
+				</div>
+			</div>
+		</div>
+	)
 }
