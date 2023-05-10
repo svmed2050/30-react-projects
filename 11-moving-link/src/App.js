5dc6c2513e856f38b131a09a265c0bcc3fff4e14
Pavel Sergienko <pavelsergienko7@gmail.com>
2023-05-10 21:38:05 +0300

    add size of canvas in useEffect

--- 11-moving-link/src/App.js
+++ 11-moving-link/src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect, useRef, useState } from 'react'
+import './App.css'
 
 export default function App() {
-  return (
-    <div className="app">
-      <canvas />
+	const canvasRef = useRef(null)
+	const [x, setX] = useState(0)
+	const [y, setY] = useState(0)
 
-      <div className="arrows">
-        <button>Up</button>
-        <button>Left</button>
-        <button>Down</button>
-        <button>Right</button>
-      </div>
+	useEffect(() => {
+		const context = canvasRef.current.getContext('2d')
+		context.canvas.height = window.innerHeight
+		context.canvas.width = window.innerWidth
 
-      <div className="images">
-        <img src="https://i.imgur.com/JYUB0m3.png" alt="Down" />
-        <img src="https://i.imgur.com/GEXD7bk.gif" alt="Right" />
-        <img src="https://i.imgur.com/XSA2Oom.gif" alt="Up" />
-        <img src="https://i.imgur.com/4LGAZ8t.gif" alt="Left" />
-      </div>
-    </div>
-  );
+		context.fillRect(100, 0, 100, 100)
+	}, [])
+
+	return (
+		<div className='app'>
+			<canvas ref={canvasRef} />
+
+			<div className='arrows'>
+				<button>Up</button>
+				<button>Left</button>
+				<button>Down</button>
+				<button>Right</button>
+			</div>
+
+			<div className='images'>
+				<img src='https://i.imgur.com/JYUB0m3.png' alt='Down' />
+				<img src='https://i.imgur.com/GEXD7bk.gif' alt='Right' />
+				<img src='https://i.imgur.com/XSA2Oom.gif' alt='Up' />
+				<img src='https://i.imgur.com/4LGAZ8t.gif' alt='Left' />
+			</div>
+		</div>
+	)
 }
